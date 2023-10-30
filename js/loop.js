d469e7d3b920c1bf7715c74e80d355bee359e65e
zederbear <95325867+zederbear@users.noreply.github.com>
2023-10-30 17:43:44 -0600

    changed diff to be /1000 again because /100 was too fast

--- js/loop.js
+++ js/loop.js
@@ -1,5 +1,5 @@
 function gameLoop(that) {
-  let diff = (Date.now() - that.player.lastUpdate)/100
+  let diff = (Date.now() - that.player.lastUpdate)/1000
   that.player.sand += that.player.efficiency[0].amount * that.player.efficiency[0].mult * diff
 
   that.player.lastUpdate = Date.now()
