d7bddacc626bc15ea5b8baf3475beb39de199c10
zederbear <95325867+zederbear@users.noreply.github.com>
2023-10-30 17:52:33 -0600

    Tried to add gold getting function

--- js/loop.js
+++ js/loop.js
@@ -1,6 +1,12 @@
 function gameLoop(that) {
   let diff = (Date.now() - that.player.lastUpdate)/1000
   that.player.sand += that.player.efficiency[0].amount * that.player.efficiency[0].mult * diff
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max)
+  }
+  if (getRandomInt(that.player.goldChance) == 0) {
+    that.player.gold += that.player.goldEfficiency[0].amount * that.player.goldEfficiency[0].mult * diff
+  }
 
   that.player.lastUpdate = Date.now()
 }
--- js/player.js
+++ js/player.js
@@ -2,13 +2,22 @@
 var player = {
     sand: 0,
     gold: 0,
+    goldChance: 0.25,
     buyMax: false,
     efficiency: [],
+    goldEfficiency: [],
     lastUpdate: Date.now()
 }
   
 var firstEfficiency = {
     cost: 10,
+    mult: 1,
+    amount: 1,
+    bought: 500,
+}
+
+var goldEfficiency = {
+    cost: 100,
     mult: 1,
     amount: 1,
     bought: 500,
