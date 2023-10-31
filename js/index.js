b99e70221d20f39cf087daef6e3eb48a769fa5ae
zederbear <95325867+zederbear@users.noreply.github.com>
2023-10-31 09:14:46 -0600

    Maybe the button workies now

--- js/index.js
+++ js/index.js
@@ -5,12 +5,12 @@ var app = new Vue({
     },
     methods: {
       buyMarketingGenerator(i) {
-        let g = this.player.marketing[i]
-        if (g.cost > this.player.money) return
-        this.player.money -= g.cost
-        this.player.marketing[i].cost *= 1 + (i+1)*0.25
-        this.player.marketing[i].amount += 1
-        this.player.marketing[i].bought += 1
+        let g = this.player.efficiency[i]
+        if (g.cost > this.player.sand) return
+        this.player.sand -= g.cost
+        this.player.efficiency[i].cost *= 1 + (i+1)*0.25
+        this.player.efficiency[i].amount += 1
+        this.player.efficiency[i].bought += 1
       },
       format(amount) {
         return format(amount)
